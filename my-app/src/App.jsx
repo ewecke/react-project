268576ebcf7ec5dbdaae1a0c2dc9086712ff8491
LenaIT <olena.denysova@gmail.com>
2020-09-01 15:46:25 +0200

    lena App

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -12,6 +12,7 @@ export default class App extends Component {
           color: "dark",
           id: 1,
           country: "Ireland",
+          price: "1 Euro",
           image_url:
             "https://dydza6t6xitx6.cloudfront.net/ci-guinness-extra-stout-2abc388ce9c3bb61.jpeg",
         },
@@ -20,6 +21,7 @@ export default class App extends Component {
           color: "light",
           id: 2,
           country: "Austria",
+          price: "1 Euro",
           image_url:
             "https://www.galatea.se/wp-content/uploads/2016/03/stiegl-goldbrau-500-ml.jpg",
         },
@@ -28,6 +30,7 @@ export default class App extends Component {
           color: "weiss",
           id: 3,
           country: "Germany",
+          price: "1 Euro",
           image_url:
             "https://www.paulaner.com/content/8-us/2-our-products/1-hefe-weizen/modules/1-product_intro-1uh606t/bottle-usa-1000x1550-hwbn-1.png",
         },
@@ -36,6 +39,7 @@ export default class App extends Component {
           color: "light",
           id: 4,
           country: "Czech republic",
+          price: "1 Euro",
           image_url:
             "https://i1.wp.com/nlliquor.com/wp-content/uploads/2020/05/7230_m_v4.jpg?fit=960%2C1280&ssl=1",
         },
@@ -44,15 +48,17 @@ export default class App extends Component {
           color: "dark",
           id: 5,
           country: "Germany",
-          img_url:
-            "https://www.carlsberggroup.com/media/22851/de_duckstein-original.png",
+          price: "1 Euro",
+          image_url:
+            "https://cdn02.plentymarkets.com/q7p0kwea05gv/item/images/3953/full/24138.png",
         },
         {
           name: "Staropramen ",
           color: "light",
           id: 6,
           country: "Czech republic",
-          img_url:
+          price: "1 Euro",
+          image_url:
             "https://cdn02.plentymarkets.com/99cbvkn2wswt/item/images/409/full/Staropramen-Lager.jpg",
         },
         {
@@ -60,7 +66,8 @@ export default class App extends Component {
           color: "light",
           id: 7,
           country: "Germany",
-          img_url:
+          price: "1 Euro",
+          image_url:
             "https://cdn02.plentymarkets.com/q7p0kwea05gv/item/images/6489/full/22353.png",
         },
         {
@@ -68,7 +75,8 @@ export default class App extends Component {
           color: "light",
           id: 8,
           country: "Mexico",
-          img_url:
+          price: "1 Euro",
+          image_url:
             "https://cdn02.plentymarkets.com/q7p0kwea05gv/item/images/6480/full/20571.png",
         },
         {
@@ -76,7 +84,8 @@ export default class App extends Component {
           color: "light",
           id: 9,
           country: "Denmark",
-          img_url:
+          price: "1 Euro",
+          image_url:
             "https://5.imimg.com/data5/QR/FB/QZ/SELLER-11737895/tuborg-non-alcohoilc-beer-500x500.jpg",
         },
         {
@@ -84,15 +93,17 @@ export default class App extends Component {
           color: "light",
           id: 10,
           country: "Ukraine",
-          img_url:
-            "https://res.cloudinary.com/ratebeer/image/upload/w_250,c_limit/beer_4530.jpg",
+          price: "1 Euro",
+          image_url:
+            "https://cheersclub.co.nz/wp-content/uploads/2018/09/premium1.jpg",
         },
         {
           name: "Zagovor",
           color: "light",
           id: 11,
           country: "Russia",
-          img_url:
+          price: "1 Euro",
+          image_url:
             "https://mir-s3-cdn-cf.behance.net/project_modules/fs/0240c526441469.5844657dc78f8.png",
         },
         {
@@ -100,7 +111,8 @@ export default class App extends Component {
           color: "light",
           id: 12,
           country: "Germany",
-          img_url:
+          price: "1 Euro",
+          image_url:
             "https://images-na.ssl-images-amazon.com/images/I/71s-0QryNxL._SL1500_.jpg",
         },
       ],
@@ -109,20 +121,23 @@ export default class App extends Component {
   render() {
 
     return (
-
+    
       this.state.biers.map((bier) => {
         return(
           <div className="products_wrapper">
+          
             <Card
               key={bier.id}
               name={bier.name}
               color={bier.color}
               country={bier.country}
+              price={bier.price}
               img_url={bier.image_url}
             />
           </div>
-      )
-    })
+        )
+      })
+      
     )
 
   }
