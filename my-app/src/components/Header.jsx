52d8f4802952a0292301b08872fa52c571de16ec
ewecke <edgargandolfi@gmail.com>
2020-09-03 10:44:40 +0200

    header-edgar

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -5,11 +5,15 @@ import logo from '../img/logo.png'
 const Header = ({length}) => {
   return (
     <header>
-      <h1 className="header">Make your own<img className="logo" src={logo} alt="Logo"/>Octoberfest 2020</h1>
-      <Link to ="/" >Home</Link>      
-      <Link to = "/cart">Go to cart {length}</Link>
-      <Link to = "/about">About</Link>
-      <Link to = "./help">Help</Link>
+      <nav className="top-menu">
+        <div><Link to = "/about">About</Link></div>
+        <div><Link to = "./help">Help</Link></div>
+        <div><Link to = "/cart">Go to cart {length}</Link></div>
+      </nav>
+      <h1 className="header">Make your own<Link to ="/" ><img className="logo" src={logo} alt="Logo"/></Link>Octoberfest 2020</h1>  
+  
+
+
     </header>
   );
 
