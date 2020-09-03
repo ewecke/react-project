d5f74c7fa57c593fa759a68eeba5843d40c36670
wuberde <wuberde@gmail.com>
2020-09-03 09:10:18 +0200

    styling

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import {Link} from "react-router-dom";
-import logo from '../img/logo.png'
+import { Link } from 'react-router-dom';
+import logo from '../img/logo.png';
+import brezel from '../img/brezel.png';
 
-const Header = ({length}) => {
+const Header = ({ length }) => {
   return (
     <header>
-      <h1 className="header">Make your own<img className="logo" src={logo} alt="Logo"/>Octoberfest 2020</h1>
-      <Link to ="/" >Home</Link>      
-      <Link to = "/cart">Go to cart {length}</Link>
-      <Link to = "/about">About</Link>
-      <Link to = "./help">Help</Link>
+      <h1 className="header">
+        <img className="brezel" src={brezel} alt="brezel" />
+        <img className="brezel" src={brezel} alt="brezel" />
+        Make your own
+        <img className="logo" src={logo} alt="Logo" />
+        Octoberfest 2020
+        <img className="brezel" src={brezel} alt="brezel" />
+      </h1>
+      <div className="nav-bar">
+        <Link to="/">Home  </Link>
+        <Link to="/cart">Go to cart {length}  </Link>
+        <Link to="/about">About  </Link>
+        <Link to="./help">Help</Link>
+      </div>
     </header>
   );
-
 };
 
 export default Header;
--- my-app/src/components/Main.jsx
+++ my-app/src/components/Main.jsx
@@ -1,52 +1,56 @@
-import React, {Component} from 'react'
-import {BrowserRouter, Switch, Route} from "react-router-dom"
-import data from "../Data/data.json"
-import App from "../App"
-import Cart from "./Cart"
-import Header from "./Header"
-import About from './About'
-import Help from "./Help"
-import NotFound from "./NotFound"
-
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import data from '../Data/data.json';
+import App from '../App';
+import Cart from './Cart';
+import Header from './Header';
+import About from './About';
+import Help from './Help';
+import NotFound from './NotFound';
+import Footer from './Footer';
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: data,
+      cart: [],
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          data: data,
-          cart: [] 
-        };
-      }
+  addToCart = (id) => {
+    const temp = this.state.data.filter((bier) => bier.id === id);
+    console.log(temp);
+    this.setState({ cart: [...this.state.cart, temp[0]] });
+  };
 
-      addToCart = (id) => {
-        const temp = this.state.data.filter(bier => bier.id === id);
-        console.log(temp);
-        this.setState({cart: [...this.state.cart, temp[0]]})
-      }
-
-    render () {
-
+  render() {
     return (
-       <BrowserRouter>
-       <Header length={this.state.cart.length}/>
+      
+      <BrowserRouter>
+        <Header length={this.state.cart.length} />
         <Switch>
-        
-        <Route path="/" exact>
-            <App data={this.state.data} addToCart={this.addToCart} cart={this.state.cart} />      
-        </Route>
+          <Route path="/" exact>
+            <App
+              data={this.state.data}
+              addToCart={this.addToCart}
+              cart={this.state.cart}
+            />
+          </Route>
 
-        <Route path="/about" component={About} exact/>
+          <Route path="/about" component={About} exact />
 
-        <Route path="/cart" exact>
-            <Cart cart={this.state.cart}/>
-        </Route>
+          <Route path="/cart" exact>
+            <Cart cart={this.state.cart} />
+          </Route>
 
-        <Route path="/help" component={Help} exact/>
-        <Route component={NotFound} />
+          <Route path="/help" component={Help} exact />
+          <Route component={NotFound} />
 
-        </Switch>        
-        </BrowserRouter>
-        )
-    }
+          
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    );
+  }
 }
