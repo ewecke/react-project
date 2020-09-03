8910ae0446765825af4018518f50c619ee87573c
LenaIT <olena.denysova@gmail.com>
2020-09-03 11:24:33 +0200

    commit with all functions

--- my-app/src/components/CartItem.jsx
+++ my-app/src/components/CartItem.jsx
@@ -6,13 +6,13 @@ export default function CartItem({item, removeFromCart}) {
 
     return (
             <div className="productCard" key={item.id}>
-                <img src={item.img_url} alt="bier sort" className="bier-img"/>
+                <img src={`${item.image_url}`} alt="bier sort" className="bier-img"/>
                 <h3 className="bier-name">{item.name}</h3>
                 <p className="bier-color">{item.color}</p>
                 <p className="bier-country">country: {item.country}
                 </p>
                 <p>price: {item.price}</p>
-                <button onClick={() => {removeFromCart(item.id)}}>X</button>
+                <button onClick={() => {removeFromCart(item.id)}}>&#128465;</button>
             </div>
     )
 }
